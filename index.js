4ba29786f23c0d38ffda9ad7eba0ac390620b6b9
Edet John <111274485+cyberohn2@users.noreply.github.com>
2023-05-18 01:05:42 -1200

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ let answer = document.getElementById('answer-update');
 let cancel = document.getElementById('cancel');
 let clear = document.getElementById('clear');
 let equalTo = document.getElementById('equalto');
-
+let operatorHandler;
 clear.addEventListener('click', () => {
-    
+   operations.value = operations.value.slice(0,-1)
+   operatorHandler = operations.value;
+   answer.value = eval(operatorHandler);
 })
  
 cancel.addEventListener('click', () => {
@@ -17,17 +19,48 @@ cancel.addEventListener('click', () => {
 })
 
 operators.forEach(display);
+const divOperator = document.querySelector('.divide')
+divOperator.addEventListener('click', e =>{
+    e.preventDefault()
+    operations.value += divOperator.getAttribute('data-operator')
+})
 
+const timesOperator = document.querySelector('.times')
+timesOperator.addEventListener('click', e =>{
+    e.preventDefault()
+    operations.value += timesOperator.getAttribute('data-operator')
+})
 
 function display (element) {
     element.addEventListener('click', () => {
         operations.value += element.value
+        operatorHandler = operations.value;
+        answer.value = eval(operatorHandler);
     });
 }
-let operatorHandler;
 
 equalTo.addEventListener('click', () => {
     operatorHandler = operations.value;
     answer.value = eval(operatorHandler);
 
+})
+
+
+window.addEventListener('keydown', e =>{
+    e.preventDefault()
+    console.log(e);
+    if (!isNaN(e.key) || e.key == '*' || e.key == '-' || e.key == '+' || e.key == '/') {
+        operations.value += e.key
+        operatorHandler = operations.value;
+        answer.value = eval(operatorHandler);
+    }
+    if (e.key == 'Backspace') {
+        operations.value = operations.value.slice(0,-1)
+        operatorHandler = operations.value;
+        answer.value = eval(operatorHandler);
+    }
+    if (e.key == 'Enter' || e.key == '=') {
+        operatorHandler = operations.value;
+        answer.value = eval(operatorHandler);
+    }
 })
